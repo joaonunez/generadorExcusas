c25717dcd88c2181ebcb306ff83e5b31a05b7dbf
joao nuñez valdiglesias <104280636+joaonunez@users.noreply.github.com>
2024-05-09 12:59:09 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,30 +1,27 @@
-
 let generarOracion = function(){
     let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
     let action = ['ate', 'peed', 'crushed', 'broke'];
     let what = ['my homework', 'my phone', 'the car'];
     let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
     let oracion = '';
-
     for (let i = 1 ; i <= 4; i++){
         if(i == 1){
             let randomNumberwho = Math.floor(Math.random()*who.length);
-            oracion += who[randomNumberwho];
+            oracion += who[randomNumberwho] + ' ';
         }
         else if(i == 2){
             let randomNumberaction = Math.floor(Math.random()*action.length);
-            oracion += action[randomNumberaction];
+            oracion += action[randomNumberaction] + ' ';
         }
         else if(i == 3){
             let randomNumberwhat = Math.floor(Math.random()*what.length);
-            oracion += what[randomNumberwhat];
+            oracion += what[randomNumberwhat] + ' ';
         }
         else{
             let randonNumberwhen = Math.floor(Math.random()*when.length);
-            oracion += when[randonNumberwhen];
+            oracion += when[randonNumberwhen]+ '.';
         }
     }
-
     document.getElementById("excuse").innerHTML = oracion
 }
 generarOracion()
