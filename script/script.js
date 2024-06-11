8c0544601dbff22710f85b6c38eef4f0b90f1cda
joaodev <joaovaldiglesias@gmail.com>
2024-06-11 00:34:15 -0400

    Generador de excusas mejorado en css e identacion

--- script/script.js
+++ script/script.js
@@ -3,25 +3,28 @@ let generarOracion = function(){
     let action = ['ate', 'peed', 'crushed', 'broke'];
     let what = ['my homework', 'my phone', 'the car'];
     let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    let espacio = " ";
     let oracion = '';
+
     for (let i = 1 ; i <= 4; i++){
         if(i == 1){
-            let randomNumberwho = Math.floor(Math.random()*who.length);
-            oracion += who[randomNumberwho] + ' ';
+            let randomNumberWho = Math.floor(Math.random()*who.length);
+            oracion += who[randomNumberWho] + espacio;
         }
         else if(i == 2){
-            let randomNumberaction = Math.floor(Math.random()*action.length);
-            oracion += action[randomNumberaction] + ' ';
+            let randomNumberAction = Math.floor(Math.random()*action.length);
+            oracion += action[randomNumberAction] + espacio;
         }
         else if(i == 3){
-            let randomNumberwhat = Math.floor(Math.random()*what.length);
-            oracion += what[randomNumberwhat] + ' ';
+            let randomNumberWhat = Math.floor(Math.random()*what.length);
+            oracion += what[randomNumberWhat] + espacio;
         }
         else{
-            let randonNumberwhen = Math.floor(Math.random()*when.length);
-            oracion += when[randonNumberwhen]+ '.';
+            let randonNumberWhen = Math.floor(Math.random()*when.length);
+            oracion += when[randonNumberWhen]+".";
         }
     }
+
     document.getElementById("excuse").innerHTML = oracion
 }
 generarOracion()
